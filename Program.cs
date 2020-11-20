45e4eaac277798a2dde92464ba4d1fc1b5a7824e
Martin Martiniak <martiniak.martin15@gmail.com>
2020-11-20 17:16:04 +0100

    Change the console greeting message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace OOP_10._00
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Random greeting message!");
         }
     }
 }

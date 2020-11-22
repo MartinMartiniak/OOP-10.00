ae916794fd18eaf43727b7116956318bb210543b
Martin Martiniak <martiniak.martin15@gmail.com>
2020-11-22 20:09:07 +0100

    Make the program look good :D

--- Book.cs
+++ Book.cs
@@ -7,18 +7,27 @@ namespace OOP_10._00
     class Book
     {
         public string Title { get; set; }
+       
         public int Pages { get; set; }
+       
         public string Category { get; set; }
+       
         public string Author { get; set; }
+      
         public int ReleaseDate { get; set; }
 
 
         public Book(string title, int pages, string category, string author, int releaseDate)
         {
+           
             Title = title;
+           
             Pages = pages;
+            
             Category = category;
+           
             Author = author;
+           
             ReleaseDate = releaseDate;
         }
         public override string ToString()
--- Program.cs
+++ Program.cs
@@ -12,24 +12,41 @@ namespace OOP_10._00
         
         Console.Write("Title: ");
             string title = Console.ReadLine();
-        Console.Write("Pages: ");
+        
+            
+            Console.Write("Pages: ");
             int pages = int.Parse(Console.ReadLine());
             if (pages < 0)
             {
                 pages = 1;
             }
-        Console.Write("Category: ");
+        
+            
+            
+            Console.Write("Category: ");
             string category = Console.ReadLine();
-        Console.Write("Author: ");
+        
+            
+            
+            Console.Write("Author: ");
             string author = Console.ReadLine();
-        Console.Write("ReleaseDate: ");
+        
+            
+            
+            Console.Write("ReleaseDate: ");
             int releaseDate = int.Parse(Console.ReadLine());
 
             if (releaseDate < 1450 || releaseDate > 2021)
             {
                 releaseDate = -1;
             }
+            
+            
+            
             Book LORT = new Book(title, pages, category, author, releaseDate);
+            
+            
+            
             Console.WriteLine($"{LORT.Title} {LORT.Pages} {LORT.Category} {LORT.Author} {LORT.ReleaseDate}");
 
         } 
